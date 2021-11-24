543ed5f51aaf18a2bca28e8c9d7784b5469ece3e
kotahorii <khorii.micron@gmail.com>
2021-11-25 01:40:04 +0900

    create controller

--- app/controllers/api/v1/hotpeppers_controller.rb
+++ app/controllers/api/v1/hotpeppers_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::HotpeppersController < ApplicationController
   require 'httpclient'
 
-  def index
-    base_url = "#{ENV['SECRET_KEY']}"
+  def create
+    base_url = "#{ENV['SECRET_KEY']}" + params[:key]
     client = HTTPClient.new()
     response = client.get(base_url)
     json = JSON.parse(response.body)
     puts response.status
     puts response.body
     render json: json
+  end
+
+  private
+
+  def hotpepper_params
+    params.permit(:key)
   end
 end
